d824153facafb13ba36cebc1ffb43bb0b49ec0d1
Amritesh Rai <raianshul1997@gmail.com>
2023-05-12 20:56:16 +0530

    Init 2

---
+++ DP/0/1_knapsack.py
@@ -0,0 +1,18 @@
+wt = [1, 2, 4, 5]
+val = [5, 4, 8, 6]
+W = 5
+n = len(wt)
+dp = [[0 for _ in range(W+1)] for _ in range(n)]
+
+# base cases
+for i in range(wt[0],W+1):
+    dp[0][i] = val[0]
+
+for i in range(1,n):
+    for j in range(W+1):
+        notTaken = dp[i-1][j]
+        taken = -11111111111111
+        if wt[i] <= j:
+            taken = val[i] + dp[i-1][j-wt[i]]
+        dp[i][j] = max(notTaken,taken)
+print(dp[n-1][W])
